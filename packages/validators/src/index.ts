89831a3065b70b760a9b65bcfb01400e0297a693
Ruofan Wang <meimprankster@gmail.com>
2024-05-27 18:31:00 +0800

    setup trpc package

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -3,3 +3,8 @@ import { z } from "zod";
 export const SignInFormSchema = z.object({
   email: z.string().email(),
 });
+
+export const CreatePostSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
